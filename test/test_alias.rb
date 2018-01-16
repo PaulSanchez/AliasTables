8db6b86b8f1093d6989f3feb1a8cb7f93e1753a1
pjs <pjs@alum.mit.edu>
2018-01-16 09:07:13 -0800

    Improved testing
    Added another test case, added printing to confirm which case is being evaluated

--- test/test_alias.rb
+++ test/test_alias.rb
@@ -20,12 +20,14 @@ Dir['test/infile.*'].each do |f_name|
     inputs = line.strip.split(/[\s,;:]+/)
     x << inputs[0]
     counts[inputs[0]] = 0
-    probs << inputs[1].to_r
+    probs << Rational(inputs[1])
     n_hat = probs[-1] * nvars
     half_width = 2.5 * Math.sqrt(n_hat * (1.0 - probs[-1])) if n_hat > 0
     expected_counts[inputs[0]] = [n_hat, half_width]
   end
   f.close
+  p x
+  p probs
   begin
     at = AliasTable.new(x, probs)
     nvars.times { counts[at.generate] += 1 }
